dfea0a8d154ba9820ea3760204b7a962ad0f7f58
Dom DiCarlo <ddicarlo@uchicago.edu>
2019-08-31 15:40:29 -0500

    finished up my n_steps.py algorithm

--- n_steps.py
+++ n_steps.py
@@ -11,12 +11,12 @@
 # The basic problem, for X = {1, 2}:
 
 # First, here is a (less than optimal) recursive solution:
-def n_steps_rec(n):
-  if (n < 0): # impossible staircase!
+def n_steps_basic_rec(n):
+  if n < 0: # impossible staircase!
     return 0
-  if (n == 0): # if the staircase has no steps, there is only 1 way 
+  if n == 0: # if the staircase has no steps, there is only 1 way 
     return 1 
-  if (n == 1): # one step is also equal to 1
+  if n == 1: # one step is also equal to 1
     return 1
   else:
     return n_steps_rec(n - 1) + n_steps_rec(n - 2)
@@ -31,12 +31,12 @@ def n_steps_rec(n):
 # We can instead use a buffer to save numbers, while still
 # more or less keeping the same algorithmic solution
 
-def n_steps_iter(n):
-  if (n < 0): # impossible staircase!
+def n_steps_basic_iter(n):
+  if n < 0: # impossible staircase!
     return 0
-  if (n == 0): # if the staircase has no steps, there is only 1 way 
+  if n == 0: # if the staircase has no steps, there is only 1 way 
     return 1 
-  if (n == 1): # one step is also equal to 1
+  if n == 1: # one step is also equal to 1
     return 1
   # else, we can utilize the buffer 
   # where for all other positive integer n,
@@ -67,7 +67,49 @@ def n_steps_iter(n):
 # f(n) = f(x - x_0) + f(x - x_1) + ... f(x - x_m) [for x - x_y > 0]
 
 # and for x in X, the same thing, + 1 for the solution using one 
-# interval of x length. Put in basic english, either the sum
+# interval of x length.
+# f(n) = f(x - x_0) + f(x - x_1) + .. f(x - x_m) + 1 [for x - x_y > 0]
+
+
+# Put in basic english, either the sum
 # of all possible ways up the stairs after using one of the 
 # available intervals in X, or that sum plus one fell swoop 
 # of an interval that fits in perfectly (e.g. x = 6 for N = 6)
+
+def n_steps_gen_rec(n, set_x):
+  if (n < 0): # impossible staircase!
+    return 0
+  if (n == 0): # in this case, only one way up, do nothing!
+    return 1
+  elif (n in set_x):
+    return 1 + sum(n_steps_gen_rec(n - x, set_x)
+                   for x in set_x if n > x)
+  else:
+    return sum(n_steps_gen_rec(n - x, set_x)
+               for x in set_x if n > x)
+
+# which gives us an O(|set_x|^n) time solution
+# again, this is not the most efficient solution. we need to do
+# something iteratively to avoid so many repeated computations
+
+# We can do something like we did before, with a buffer
+# array of a bigger size.
+
+def n_steps_gen_iter(n, set_x):
+  # range n+1 since index[0] is 0 steps,
+  # index[n] is for n steps
+  buff = [0 for i in range(n + 1)]
+  buff[0] = 1
+  for i in range(n + 1):
+    # essentially works out the base cases
+    # for each member of the set_x
+    if (i in set_x):
+      buff[i] += 1 
+    # computes using our algorithm
+    buff[i] += sum(buff[i - x] for x in set_x if i - x > 0)
+  return buff[-1]
+
+base_set = [1, 2]
+
+print(n_steps_gen_iter(0, base_set))
+
