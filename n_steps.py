aff2281431e01b756fa1ff591384840ac5708132
Dom DiCarlo <ddicarlo@uchicago.edu>
2019-08-30 19:57:15 -0500

    initial commit for readme and part one of the n_steps problem

---
+++ n_steps.py
@@ -0,0 +1,73 @@
+# The N Steps Problem 
+
+# The problem: Given a staircase of N number of steps, 
+# if you can only take intervals of 1 or 2 steps, what 
+# is the total number of permutations to get to the top?
+
+# Further, extend this to a set of intervals X. For a given
+# set of positive integers, what is the total number of
+# permutations to get to the top?
+
+# The basic problem, for X = {1, 2}:
+
+# First, here is a (less than optimal) recursive solution:
+def n_steps_rec(n):
+  if (n < 0): # impossible staircase!
+    return 0
+  if (n == 0): # if the staircase has no steps, there is only 1 way 
+    return 1 
+  if (n == 1): # one step is also equal to 1
+    return 1
+  else:
+    return n_steps_rec(n - 1) + n_steps_rec(n - 2)
+
+# The basic idea is that at any point I can either take 1 step, or 
+# 2 steps, so I follow those possible futures and add up their
+# endings.
+
+# This will take O(2^n) time, however. We are calculating the 
+# same things multiple times.
+
+# We can instead use a buffer to save numbers, while still
+# more or less keeping the same algorithmic solution
+
+def n_steps_iter(n):
+  if (n < 0): # impossible staircase!
+    return 0
+  if (n == 0): # if the staircase has no steps, there is only 1 way 
+    return 1 
+  if (n == 1): # one step is also equal to 1
+    return 1
+  # else, we can utilize the buffer 
+  # where for all other positive integer n,
+  # f(n) = f(n-2) + f(n-1)
+  else:
+    buff = [1, 1, 1]
+    for x in range (n - 1):
+      buff[2] = buff[0] + buff[1]
+      buff[0] = buff[1]
+      buff[1] = buff[2]
+    return buff[2]
+
+# This changes our problem to one of O(n) time
+
+# Now, we need to come up with a solution
+# for a general set of numbers. 
+
+# For x_0 to x_m in X, any given f(x) will be the
+# summation of f(x - x_y) for y from 0 to x_m, using
+# the same logic as our previous problem (following all possible
+# futures, all possible step intervals from our vantage point). 
+# For a more general solution, things will be somewhat tricker,
+# since some values for N steps won't have any way up with
+# our limits of intervals (e.g. take only even intervals for an odd N)
+
+# Since we can't work out the base cases in advance, we must 
+# change our solution some what. Rather for x not in X,
+# f(n) = f(x - x_0) + f(x - x_1) + ... f(x - x_m) [for x - x_y > 0]
+
+# and for x in X, the same thing, + 1 for the solution using one 
+# interval of x length. Put in basic english, either the sum
+# of all possible ways up the stairs after using one of the 
+# available intervals in X, or that sum plus one fell swoop 
+# of an interval that fits in perfectly (e.g. x = 6 for N = 6)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Coding Algorithms 
+## By Dominic DiCarlo
+
+These enclosed .py files are solutions worked out by me (that I have often consulted online resources to come up with) for popular coding/math problems found in programming interviews. I have started this repo to practice coding these answers for myself, and to put them somewhere I can refer to them again while studying. I have tried to add commented explanations where appropriate. 
+
+If they help you, fantastic!
